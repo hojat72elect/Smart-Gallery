d3ad73cee70173043b165825a345586b8bd40458
hojat <hojat72elect@gmail.com>
2024-07-29 18:09:40 -0400

    Moved Application extensions to a single class.

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/App.kt
@@ -2,9 +2,11 @@ package com.simplemobiletools.gallery.pro
 
 import android.app.Application
 import com.github.ajalt.reprint.core.Reprint
-import com.simplemobiletools.gallery.pro.extensions.checkUseEnglish
+import com.simplemobiletools.gallery.pro.extensions.baseConfig
+import com.simplemobiletools.gallery.pro.helpers.isNougatPlus
 import com.squareup.picasso.Downloader
 import com.squareup.picasso.Picasso
+import java.util.Locale
 import okhttp3.Request
 import okhttp3.Response
 
@@ -18,5 +20,14 @@ class App : Application() {
 
             override fun shutdown() {}
         }).build())
+    }
+
+    private fun checkUseEnglish() {
+
+        if (baseConfig.useEnglish && !isNougatPlus()) {
+            val conf = resources.configuration
+            conf.locale = Locale.ENGLISH
+            resources.updateConfiguration(conf, resources.displayMetrics)
+        }
     }
 }
