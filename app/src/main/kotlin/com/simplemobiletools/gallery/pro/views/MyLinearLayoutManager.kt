23c10fbfbecf75a34dae4bb4b57b433c2b0b7d00
hojat <hojat72elect@gmail.com>
2024-07-30 18:23:31 -0400

    Fixed the runtime error caused by the lack of constructors in MyLinearLayoutManager.

--- app/src/main/kotlin/com/simplemobiletools/gallery/pro/views/MyLinearLayoutManager.kt
+++ app/src/main/kotlin/com/simplemobiletools/gallery/pro/views/MyLinearLayoutManager.kt
@@ -1,10 +1,18 @@
 package com.simplemobiletools.gallery.pro.views
 
 import android.content.Context
+import android.util.AttributeSet
 import androidx.recyclerview.widget.LinearLayoutManager
 
-class MyLinearLayoutManager(context: Context) : LinearLayoutManager(context) {
+/**
+ * Warning : never remove any of these constructors, otherwise, you will face runtime errors.
+ */
+class MyLinearLayoutManager : LinearLayoutManager {
+    constructor(context: Context) : super(context)
 
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    constructor(context: Context, orientation: Int, reverseLayout: Boolean) : super(context, orientation, reverseLayout)
 
     // fixes crash java.lang.IndexOutOfBoundsException: Inconsistency detected...
     // taken from https://stackoverflow.com/a/33985508/1967672
